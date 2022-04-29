5803fec132b103f5b6b4055f0339bbc63e21852e
Sucasaire Fiestas <A446499@santandertecnologia.com.ar>
2022-04-29 11:55:17 -0300

    agregando rutas al proyecto

--- app.js
+++ app.js
@@ -21,3 +21,13 @@ app.set("view", "./views");
 // para que los formularios accepten json
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+// rutas de la pagina
+app.use(require("./routers/home"));
+app.use("/products",require("./routers/product"));
+app.use("/cart",require("./routers/cart"));
+app.use("/checkout",require("./routers/checkout"));
+app.use("/register",require("./routers/register"));
+app.use("/login",require("./routers/login"));
+
+
