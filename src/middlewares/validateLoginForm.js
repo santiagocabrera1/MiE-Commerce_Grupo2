53644aa9ab3225370bda196cc1086138d32f02e5
Cabrera <A446462@santandertecnologia.com.ar>
2022-05-06 11:07:46 -0300

    agregando validateLoginForm

---
+++ src/middlewares/validateLoginForm.js
@@ -0,0 +1,11 @@
+const { name } = require('ejs');
+const { check} = require ('express-validator');
+
+const validationsLogin = [
+    check("email")
+    .notEmpty().withMessage("Este campo es obligatorio").bail().isEmail().trim().withMessage("Ingrese un email valido porfavor"),
+    check ("password")
+    .notEmpty().withMessage("Este campo es obligatorio").trim(),
+]
+
+module.exports= validationsLogin;
