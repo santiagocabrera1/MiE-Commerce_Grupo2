9eaaf4e80ac22c920f6816c66db76cf51dcbb0c3
Sucasaire Fiestas <A446499@santandertecnologia.com.ar>
2022-05-09 15:50:49 -0300

    creando modelos para filtrar productos

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,13 @@
-const products = require("../data/products.json")
+const products = require("../data/products.json");
+const model = require("../models/products");
 
 module.exports = {
-    index: (req,res)=>{
-        res.render("./pages/products", { products });
+    index:  (req,res)=>{
+        console.log(model.findMostwanted());
+        res.render("./pages/products", {products: model.findCategory("electronica")});
     },
     indexCart: (req,res)=>{
         res.render("./pages/cart");
     }
-    
 }
 
