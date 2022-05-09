a4672b4e3e0453ae424efa7c190c779af5b72e00
Sucasaire Fiestas <A446499@santandertecnologia.com.ar>
2022-05-09 10:31:10 -0300

    arreglando links de registro

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -18,10 +18,18 @@ module.exports = {
             res.render("./pages/register", {errors : errors.mapped(), oldData: req.body});
         } else {
             let users = JSON.parse(fs.readFileSync(path.join(__dirname ,"../data/users.json"), {encoding: "utf-8"}));
+            
+            let id = 0; 
+            users.forEach(user => {
+                    id = user.id > id ? user.id: id;
+            })
+
             users.push({
+                id,
                 ...req.body,
                 password: bcrypt.hashSync(req.body.password, 10)
             });
+
             fs.writeFileSync(path.join(__dirname ,"../data/users.json"), JSON.stringify(users));
             res.redirect("/login");
 
